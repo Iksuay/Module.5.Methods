15c46826978b370c5f09f12bc71d114f064d1e5b
Tamer <tamer.masud@mail.ru>
2023-10-26 20:09:35 +0400

    Задание 1

--- Module.5.Methods/Program.cs
+++ Module.5.Methods/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+
+(string Name, string[] Dishes) User;
+
+Console.WriteLine("Введите имя пользователя");
+User.Name = Console.ReadLine();
+
+User.Dishes = new string[5];
+for (int i = 0; i< User.Dishes.Length; i++)
+{
+    Console.WriteLine("Введите блюдо номер {0}", i + 1);
+    User.Dishes[i] = Console.ReadLine();
+}
